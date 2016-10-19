046db5254c6ad49c0c592162cd5cd3de960e351f
unknown <>
2016-10-19 23:24:00 +0800

    Initial commit

---
+++ app/src/main/java/com/fangjaexi/mengmengda/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.fangjaexi.mengmengda;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/10/16.
+ */
+public class SettingsActivity extends AppCompatActivity {
+
+}
